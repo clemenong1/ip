e23f8d4e3aa23b9f222472d70beaaace0f3742cc
clemenong1 <clemenojk28@gmail.com>
2026-01-23 00:47:09 +0800

    Changed chatbot name to Bob, as well as the respective files. Editted greeting

---
+++ src/main/java/Bob.java
@@ -0,0 +1,10 @@
+public class Bob {
+    public static void main(String[] args) {
+        System.out.println("__________________________________________________");
+        System.out.println("Hello! I'm Bob");
+        System.out.println("What can I do for you?");
+        System.out.println("__________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("__________________________________________________");
+    }
+}
