6675830cf3acd014782fb57370463ad9cece11ee
clemenong1 <clemenojk28@gmail.com>
2026-02-20 10:07:30 +0800

    Add text wrapping with input bar expanding vertically
    Input text can get very long and input bar will continue to
    go leftwards with more text.
    
    Changed it to expand vertically so that text can still be seen
    even if the text gets too long.

--- src/main/java/bob/gui/Main.java
+++ src/main/java/bob/gui/Main.java
@@ -22,6 +22,7 @@ public class Main extends Application {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
+            scene.getStylesheets().add(Main.class.getResource("/view/compact-textarea.css").toExternalForm());
             stage.setScene(scene);
             stage.setTitle("Bob Chatbot");
             fxmlLoader.<MainWindow>getController().setBob(bob);
