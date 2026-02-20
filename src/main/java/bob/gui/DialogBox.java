3ea5bfa716bb31065f352863781860356fdb6c83
clemenong1 <clemenojk28@gmail.com>
2026-02-20 10:27:21 +0800

    Add personality of Bob to be a sunny and happy vibe
    Colors of the chatbot interface, chat bubble, and input bar
    should fit the theme

--- src/main/java/bob/gui/DialogBox.java
+++ src/main/java/bob/gui/DialogBox.java
@@ -14,10 +14,12 @@ import javafx.scene.layout.Priority;
  * A custom control representing a dialog box consisting of a label and an image view.
  */
 public class DialogBox extends HBox {
-    private static final String BUBBLE_STYLE =
-            "-fx-background-color: #e8e8e8; -fx-background-radius: 10; -fx-padding: 10;";
+    private static final String BOB_BUBBLE_STYLE =
+            "-fx-background-color: #A5D6A7; -fx-background-radius: 10; -fx-padding: 10;";
+    private static final String USER_BUBBLE_STYLE =
+            "-fx-background-color: #BDBDBD; -fx-background-radius: 10; -fx-padding: 10;";
     private static final String ERROR_STYLE =
-            "-fx-background-color: #ffcccc; -fx-background-radius: 10; -fx-padding: 10;";
+            "-fx-background-color: #EF9A9A; -fx-background-radius: 10; -fx-padding: 10;";
 
     private Label text;
     private ImageView displayPicture;
@@ -27,24 +29,17 @@ public class DialogBox extends HBox {
      *
      * @param s Text to display.
      * @param i Image to display.
-     */
-    public DialogBox(String s, Image i) {
-        this(s, i, false);
-    }
-
-    /**
-     * Creates a dialog box with the given text, image, and optional error styling.
-     *
-     * @param s Text to display.
-     * @param i Image to display.
      * @param isError If true, the bubble is styled in red to indicate an error.
+     * @param isUser If true, the bubble is styled for user; otherwise for Bob.
      */
-    public DialogBox(String s, Image i, boolean isError) {
+    public DialogBox(String s, Image i, boolean isError, boolean isUser) {
         text = new Label(s);
         text.setWrapText(true);
         text.setMaxWidth(Double.MAX_VALUE);
         HBox.setHgrow(text, Priority.ALWAYS);
-        text.setStyle(isError ? ERROR_STYLE : BUBBLE_STYLE);
+        String bubbleStyle = isError ? ERROR_STYLE
+                : (isUser ? USER_BUBBLE_STYLE : BOB_BUBBLE_STYLE);
+        text.setStyle(bubbleStyle);
 
         displayPicture = new ImageView(i);
         displayPicture.setFitWidth(100.0);
@@ -65,7 +60,7 @@ public class DialogBox extends HBox {
     }
 
     public static DialogBox getUserDialog(String text, Image img) {
-        return new DialogBox(text, img);
+        return new DialogBox(text, img, false, true);
     }
 
     public static DialogBox getBobDialog(String text, Image img) {
@@ -81,7 +76,7 @@ public class DialogBox extends HBox {
      * @return A dialog box for Bob's response.
      */
     public static DialogBox getBobDialog(String text, Image img, boolean isError) {
-        var db = new DialogBox(text, img, isError);
+        var db = new DialogBox(text, img, isError, false);
         db.flip();
         return db;
     }
